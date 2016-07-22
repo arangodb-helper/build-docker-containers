bbcdf9b3b8c19c0af66b9de21855404ef5a1d106
Wilfried Goesgens <willi@arangodb.com>
2016-07-22 17:43:49 +0200

    call scl more early

--- distros/centos/6/scripts/install.sh
+++ distros/centos/6/scripts/install.sh
@@ -12,4 +12,6 @@ yum -y install tar wget bzip2 git cmake automake autoconf
 # get the latest gcc:
 # https://www.softwarecollections.org/en/scls/rhscl/devtoolset-3/
 yum -y install centos-release-scl
-yum -y --nogpgcheck install devtoolset-3-gcc devtoolset-3-gcc-c++ 
+yum -y --nogpgcheck install devtoolset-3-gcc devtoolset-3-gcc-c++
+echo "running scl enable: "
+scl enable devtoolset-3 bash
