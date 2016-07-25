e631bcef092e2319593056a7d3daf684a9f9b29c
Wilfried Goesgens <willi@arangodb.com>
2016-07-25 13:17:41 +0200

    work with the arangodb gcc yum repo

--- distros/centos/6/scripts/install.sh
+++ distros/centos/6/scripts/install.sh
@@ -17,7 +17,7 @@ yum -y install tar wget bzip2 git cmake automake autoconf
 # scl enable devtoolset-3 bash
 
 cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_CentOS-6/home:fceller2.repo
-yum -y install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel glibc-devel openssl-devel
+yum -y install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel glibc-devel python-argparse openssl-devel 
 
 
 echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
