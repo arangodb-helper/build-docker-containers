f2d89ccc66d4cd7b3a1cbc8cd07d3e73f06d3b82
Wilfried Goesgens <willi@arangodb.com>
2016-09-08 19:45:38 +0200

    Add first try for centos 7

---
+++ distros/centos/7/run/scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if test -n "${HTTP_PROXY}"; then
+    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+fi
+
+echo "proxy=${HTTP_PROXY}"
+cat /etc/yum.conf
+# install the bare arangodb dependencies:
+yum -y install tar bzip2 openssl wget
+
+cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_CentOS-6/home:fceller2.repo
+yum -y install arangodb-jemalloc rspec
