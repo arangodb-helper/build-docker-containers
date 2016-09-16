1726cc386fa89ae0ecc2522cefb928b44613547f
Wilfried Goesgens <willi@arangodb.com>
2016-09-16 12:43:56 +0200

    next chunk of scripts

--- scripts/copyFileLockedIfNewer.py
+++ scripts/copyFileLockedIfNewer.py
@@ -5,11 +5,11 @@ import sys
 
 my_md5sum=sys.argv[1]
 my_sourcefile=sys.argv[2]
-my_directory=sys.argv[3]
+my_lockfile=sys.argv[3]
 my_destfile=sys.argv[4]
 
 print("waiting for lock: \n")
-lock = LockFile(my_sourcefile)
+lock = LockFile(my_lockfile)
 while not lock.i_am_locking():
     try:
         lock.acquire(timeout=600)    # wait up to 10 Minutes
