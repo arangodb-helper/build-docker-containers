5c2a645848d14f41809c12296a092d59ad0bc124
Wilfried Goesgens <willi@arangodb.com>
2016-09-16 12:45:42 +0200

    fix distribute script.

--- scripts/spreadScripts.sh
+++ scripts/spreadScripts.sh
@@ -6,8 +6,8 @@ for targetDir in `find ../distros -type d  |grep scripts`; do
     for file in *; do
         if test "$file" != "$thisScript"; then
             cp $file $targetDir
-            git add $file
-            files="$files $file"
+            git add $targetDir/$file
+            files="$files $targetDir/$file"
         fi
     done
 done
