e770b38862bf496ea3211453472ac8b0ec163d84
Wilfried Goesgens <willi@arangodb.com>
2016-09-19 15:54:09 +0200

    fix arguments sequence.

--- mesosphere-jenkins-dind/push_to_local_docker_registry.sh
+++ mesosphere-jenkins-dind/push_to_local_docker_registry.sh
@@ -2,6 +2,6 @@
 
 cd ..
 REGISTRY=$1
-docker build  -t mesosphere-jenkins-dind arangodb/mesosphere_jenkins
+docker build mesosphere-jenkins-dind -t arangodb/mesosphere_jenkins
 docker tag arangodb/mesosphere_jenkins ${REGISTRY}/arangodb/mesosphere_jenkins
 docker push ${REGISTRY}/arangodb/mesosphere_jenkins
