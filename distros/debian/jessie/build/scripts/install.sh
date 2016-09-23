b1727e73448047abc45df6b29dd3eaef78615dcc
Wilfried Goesgens <willi@arangodb.com>
2016-09-23 16:14:13 +0200

    Add debian jessie buildcontainers

---
+++ distros/debian/jessie/build/scripts/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+if test -n "${HTTP_PROXY}"; then
+    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+fi
+
+echo "proxy=${HTTP_PROXY}"
+apt-get update
+apt-get install -y tar wget bzip2 git cmake automake autoconf python-argparse build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev python python-lockfile
+
+cd /tmp/
+wget http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0//Release.key
+apt-key add - < Release.key
+rm Release.key
+
+echo 'deb http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0/ /' | tee /etc/apt/sources.list.d/arangodbbuild.list
+apt-get update
+apt-get install -y arangodb-gcc54
+
+
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/profile
---
+++ distros/debian/jessie/resilience/scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+if test -n "${HTTP_PROXY}"; then
+    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+fi
+
+echo "proxy=${HTTP_PROXY}"
+apt-get update
+apt-get install -y \
+        tar wget curl bzip2 \
+        build-essential apt-utils apt-transport-https \
+        git cmake automake autoconf \
+        libjemalloc1 openssl
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/profile
