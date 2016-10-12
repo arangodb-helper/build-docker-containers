dd638ec054bfda45f0a8aa223a44af1501dfab4f
Wilfried Goesgens <willi@arangodb.com>
2016-10-12 10:20:44 +0200

    fedora uses dnf

--- distros/fedora/23/build/scripts/install.sh
+++ distros/fedora/23/build/scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-if test -n "${HTTP_PROXY}"; then
-    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
-fi
+#if test -n "${HTTP_PROXY}"; then
+#    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+#fi
 
 echo "proxy=${HTTP_PROXY}"
-cat /etc/yum.conf
-yum -y install tar wget bzip2 git cmake make automake autoconf python-argparse openssl-devel rpm-build
+#cat /etc/yum.conf
+dnf -y install tar wget bzip2 git cmake make automake autoconf python-argparse openssl-devel rpm-build
 
 # get the latest gcc:
 # https://www.softwarecollections.org/en/scls/rhscl/devtoolset-3/
@@ -16,8 +16,8 @@ yum -y install tar wget bzip2 git cmake make automake autoconf python-argparse o
 # echo "running scl enable: "
 # scl enable devtoolset-3 bash
 
-cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_CentOS-6/home:fceller2.repo
-yum -y install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel arangodb-jemalloc-devel-static glibc-devel
+mkdir -p /etc/yum.repos.d; cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_CentOS-6/home:fceller2.repo
+dnf -y install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel arangodb-jemalloc-devel-static glibc-devel
 
 
 echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
--- distros/fedora/23/run/scripts/install.sh
+++ distros/fedora/23/run/scripts/install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-if test -n "${HTTP_PROXY}"; then
-    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
-fi
+#if test -n "${HTTP_PROXY}"; then
+#    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+#fi
 
 echo "proxy=${HTTP_PROXY}"
-cat /etc/yum.conf
+#cat /etc/yum.conf
 # install the bare arangodb dependencies:
-yum -y install tar bzip2 openssl wget
+dnf -y install tar bzip2 openssl wget
 
-cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_CentOS-6/home:fceller2.repo
-yum -y install rspec
+mkdir -p /etc/yum.repos.d; cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_CentOS-6/home:fceller2.repo
+dnf -y install rspec
