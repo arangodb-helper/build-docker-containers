25c4b5c1121f5c8be18bc4d196e755b3164142e6
Wilfried Goesgens <willi@arangodb.com>
2016-10-26 16:10:41 +0200

    Add self compiled cmake

--- distros/opensuse/13/build/scripts/install.sh
+++ distros/opensuse/13/build/scripts/install.sh
@@ -7,16 +7,20 @@ fi
 
 echo "proxy=${HTTP_PROXY}"
 zypper refresh
-zypper -n install tar wget bzip2 git cmake automake autoconf python-argparse ca-certificates-mozilla
+zypper -n install tar wget bzip2 git automake autoconf python-argparse ca-certificates-mozilla
 
 zypper -n addrepo http://download.opensuse.org/repositories/home:/fceller2/openSUSE_13.1/home:fceller2.repo
+# cmake: 
+zypper -n addrepo http://download.opensuse.org/repositories/home:/dothebart:/branches:/devel:/tools:/building/openSUSE_13.2/
 
 wget http://download.opensuse.org/repositories/home:/fceller2/openSUSE_13.1/repodata/repomd.xml.key
 rpm --import repomd.xml.key
-
+# cmake:
+wget http://download.opensuse.org/repositories/dothebart:/branches:/devel:/tools:/building/openSUSE_13.2/repodata/repomd.xml.key
+rpm --import repomd.xml.key
 
 zypper -n refresh
-zypper -n install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel arangodb-jemalloc-devel-static glibc-devel openssl-devel rpm-build
+zypper -n install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel arangodb-jemalloc-devel-static glibc-devel openssl-devel rpm-build  cmake
 
 useradd jenkins -u 1000
 
