9306fa1f9d357dfc2788eaa48cd0bc05fd31e29a
Wilfried Goesgens <willi@arangodb.com>
2016-10-27 13:52:55 +0200

    add key handling

--- distros/centos/7/build/scripts/install.sh
+++ distros/centos/7/build/scripts/install.sh
@@ -17,8 +17,17 @@ yum -y install tar wget bzip2 git make automake autoconf python-argparse openssl
 # scl enable devtoolset-3 bash
 
 cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_7/home:fceller2.repo
+wget http://download.opensuse.org/repositories/home:/fceller2/CentOS_7/home:fceller2.repo/repodata/repomd.xml.key
+rpm --import repomd.xml.key
+rm -f repomd.xml.key
 cd /etc/yum.repos.d; wget http://download.opensuse.org/repositories/home:/dothebart:/branches:/devel:/tools:/building/CentOS_7/home:dothebart:branches:devel:tools:building.repo
+wget http://download.opensuse.org/repositories/home:/dothebart:/branches:/devel:/tools:/building/CentOS_7/home:dothebart:branches:devel:tools:building.repo/repodata/repomd.xml.key
+rpm --import repomd.xml.key
+rm -f repomd.xml.key
+
+
 yum -y install arangodb-gcc54-5.4.0 arangodb-jemalloc-devel arangodb-jemalloc-devel-static glibc-devel cmake 
+
 
 useradd jenkins -u 1000
 
