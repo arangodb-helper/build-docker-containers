638b38c2278a9cb12cab06017406593bfb745e55
Wilfried Goesgens <willi@arangodb.com>
2016-11-11 15:41:28 +0100

    add backports cmake

--- distros/debian/jessie/build/scripts/install.sh
+++ distros/debian/jessie/build/scripts/install.sh
@@ -7,7 +7,7 @@ fi
 
 echo "proxy=${HTTP_PROXY}"
 apt-get update
-apt-get install -y tar wget bzip2 git cmake automake autoconf python-argparse build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev python python-pip
+apt-get install -y tar wget bzip2 automake autoconf python-argparse build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev python python-pip
 
 pip install lockfile
 
@@ -16,9 +16,13 @@ wget http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0//Releas
 apt-key add - < Release.key
 rm Release.key
 
+
+http://download.opensuse.org/repositories/home:/dothebart:/branches:/devel:/tools:/building/ /home:dothebart:branches:devel:tools:building.repo
+
+echo 'deb http://ftp.debian.org/debian jessie-backports main' | tee /etc/apt/sources.list.d/backports.list
 echo 'deb http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0/ /' | tee /etc/apt/sources.list.d/arangodbbuild.list
 apt-get update
-apt-get install -y arangodb-gcc54
+apt-get install -y arangodb-gcc54 git cmake 
 
 useradd jenkins -u 1000
 
