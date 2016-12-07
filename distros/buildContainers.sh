5e5107c210b85a03377000b7fd174d2c4efd1ecd
servusoft <servusoft@users.noreply.github.com>
2016-12-07 03:58:00 +0100

    remove '-f' at docker tag ..

--- distros/buildContainers.sh
+++ distros/buildContainers.sh
@@ -10,7 +10,7 @@ function buildAndPushContainer() {
     #docker build -t ${LABEL}/${BUILDNRUN} https://github.com/arangodb-helper/build-docker-containers.git#master:distros/${DIRECTORY}/${BUILDNRUN}
     docker build -t ${LABEL}/${BUILDNRUN} ${WD}/${DIRECTORY}/${BUILDNRUN}
 
-    docker tag -f ${LABEL}/${BUILDNRUN} ${REGISTRY}/${LABEL}/${BUILDNRUN}
+    docker tag ${LABEL}/${BUILDNRUN} ${REGISTRY}/${LABEL}/${BUILDNRUN}
 
     docker push ${REGISTRY}/${LABEL}/${BUILDNRUN}
 
