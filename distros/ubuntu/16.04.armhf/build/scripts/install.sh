a5a23444bd5e19c674fa6cddaf20b16c4cee354f
Wilfried Goesgens <willi@arangodb.com>
2016-12-09 13:57:44 +0100

    add missing library for cross compiling

--- distros/ubuntu/16.04.armhf/build/scripts/install.sh
+++ distros/ubuntu/16.04.armhf/build/scripts/install.sh
@@ -50,7 +50,7 @@ apt-get install -y tar wget bzip2 git cmake automake autoconf \
 # apt-get install -y mc nano
 apt-get install -y g++-arm-linux-gnueabihf
 apt-get install -y g++-aarch64-linux-gnu
-
+apt-get install -y libstdc++6:armhf
 # for dpkg-shlibdebs we need this:
 dpkg -r gcc  g++ build-essential
 cd /usr/bin
