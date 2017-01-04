5893f7e7f3852c8dcbf0769fe762016bf392c1e6
servusoft <servusoft@users.noreply.github.com>
2017-01-04 08:00:18 +0100

    add aarch64 architercture

---
+++ distros/ubuntu/16.04.arm64/build/scripts/install.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+set -e
+
+if test -n "${HTTP_PROXY}"; then
+    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+fi
+
+echo "proxy=${HTTP_PROXY}"
+apt-get update
+
+dpkg --add-architecture arm64
+
+#sources.list
+echo 'deb-src [arch=arm64] http://ports.ubuntu.com/ xenial main restricted universe multiverse' > /etc/apt/sources.list.d/build.list
+echo 'deb-src [arch=,arm64] http://ports.ubuntu.com/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+echo 'deb-src [arch=,arm64] http://ports.ubuntu.com/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+echo 'deb-src [arch=,arm64] http://ports.ubuntu.com/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+
+echo 'deb [arch=arm64] http://ports.ubuntu.com/ xenial main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+echo 'deb [arch=arm64] http://ports.ubuntu.com/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+echo 'deb [arch=arm64] http://ports.ubuntu.com/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+echo 'deb [arch=arm64] http://ports.ubuntu.com/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list.d/build.list
+
+cat /etc/apt/sources.list.d/build.list
+
+#sources.list
+echo 'deb [arch=i386,amd64] http://archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse' > /etc/apt/sources.list
+echo 'deb [arch=i386,amd64] http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb [arch=i386,amd64] http://archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list
+
+echo 'deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src [arch=i386,amd64] http://archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list
+
+cat /etc/apt/sources.list
+
+apt-get update
+
+#apt-get purge -y libssl-dev 
+
+apt-get install -y libssl-dev:arm64
+
+apt-get install -y tar wget bzip2 git cmake automake autoconf \
+	python-argparse python python-lockfile \
+	apt-utils apt-transport-https \
+	debhelper snapcraft
+
+# apt-get install -y mc nano
+apt-get install -y g++-aarch64-linux-gnu
+apt-get install -y libstdc++6:arm64
+# for dpkg-shlibdebs we need this:
+dpkg -r gcc  g++ build-essential
+cd /usr/bin
+ln -s aarch64-linux-gnu-gcc gcc
+
+useradd jenkins -u 1000
+
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/profile
