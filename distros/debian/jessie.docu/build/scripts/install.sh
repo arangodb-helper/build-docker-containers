797af15a22dc0de2ccefa78478adb47db380742e
Wilfried Goesgens <willi@arangodb.com>
2017-03-23 14:02:01 +0100

    change sequence

--- distros/debian/jessie.docu/build/scripts/install.sh
+++ distros/debian/jessie.docu/build/scripts/install.sh
@@ -6,7 +6,28 @@ if test -n "${HTTP_PROXY}"; then
 fi
 
 echo "proxy=${HTTP_PROXY}"
+
 apt-get update
+
+apt-get install wget
+
+cd /tmp/
+wget http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0//Release.key
+apt-key add - < Release.key
+rm Release.key
+
+# First the more modern stuff so we don't need to update over packages:
+echo 'deb http://ftp.debian.org/debian jessie-backports main' | tee /etc/apt/sources.list.d/backports.list
+echo 'deb http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0/ /' | tee /etc/apt/sources.list.d/arangodbbuild.list
+
+apt-get update
+
+apt-get install -y arangodb-gcc54 
+apt-get -t jessie-backports install -y git
+apt-get -t jessie-backports install -y cmake 
+apt-get -t jessie-backports install -y git
+apt-get -t jessie-backports install -y calibre
+
 apt-get install -y tar wget bzip2 make python-argparse python-setuptools build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev python python-pip ditaa xvfb libidn11-dev daemon
 
 # work around broken binfmt_misc support:
@@ -16,19 +37,7 @@ java -jar /usr/share/ditaa/ditaa.jar $@
 ' > /usr/bin/ditaa
 chmod a+x /usr/bin/ditaa
 
-cd /tmp/
-wget http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0//Release.key
-apt-key add - < Release.key
-rm Release.key
 
-echo 'deb http://ftp.debian.org/debian jessie-backports main' | tee /etc/apt/sources.list.d/backports.list
-echo 'deb http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0/ /' | tee /etc/apt/sources.list.d/arangodbbuild.list
-apt-get update
-apt-get install -y arangodb-gcc54 
-apt-get -t jessie-backports install -y git
-apt-get -t jessie-backports install -y cmake 
-apt-get -t jessie-backports install -y git
-apt-get -t jessie-backports install -y calibre
 
 rm -f /var/cache/apt/archives/*deb || true
 rm -f /var/lib/apt/lists/* || true
