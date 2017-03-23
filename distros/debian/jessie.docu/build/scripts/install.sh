b3f05ba379f8020c482013fefa412ad89aa3fa5b
Wilfried Goesgens <willi@arangodb.com>
2017-03-23 14:41:02 +0100

    install python sh* first so it doesn't get irretated over backport stuff

--- distros/debian/jessie.docu/build/scripts/install.sh
+++ distros/debian/jessie.docu/build/scripts/install.sh
@@ -9,7 +9,7 @@ echo "proxy=${HTTP_PROXY}"
 
 apt-get update
 
-apt-get install wget
+apt-get install wget python-setuptools python python-pip python-argparse 
 
 cd /tmp/
 wget http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0//Release.key
@@ -28,7 +28,7 @@ apt-get -t jessie-backports install -y cmake
 apt-get -t jessie-backports install -y git
 apt-get -t jessie-backports install -y calibre
 
-apt-get install -y tar wget bzip2 make python-argparse python-setuptools build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev python python-pip ditaa xvfb libidn11-dev daemon
+apt-get install -y tar wget bzip2 make build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev ditaa xvfb libidn11-dev daemon
 
 # work around broken binfmt_misc support:
 rm -f /usr/bin/ditaa
