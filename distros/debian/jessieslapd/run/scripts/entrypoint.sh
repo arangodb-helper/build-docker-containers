14d8e1f360dcdf8da757a12d31a9b2a8f18b3b17
baslr <manuel@arangodb.com>
2017-04-10 11:52:34 +0200

    added slapd test container

---
+++ distros/debian/jessieslapd/run/scripts/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd /scripts
+pwd
+
+service slapd stop
+rm -rf /etc/ldap && rm -rf /var/lib/ldap && rm -rf /usr/share/slapd
+
+mkdir -p /etc/ssl/certs
+mkdir -p /etc/ssl/private
+
+mv ldap_conf  /etc/ldap
+mv ldap_db    /var/lib/ldap
+mv slapd_conf /usr/share/slapd
+
+chown -R openldap:openldap /etc/ldap/slapd.d
+chown -R openldap:openldap /var/lib/ldap
+
+# create certs
+mkdir /etc/ssl/templates
+echo "cn = $LDAP_CERT_CN" >> crypto/ldap_server.conf
+
+mv crypto/ca_server.conf   /etc/ssl/templates/ca_server.conf
+mv crypto/ldap_server.conf /etc/ssl/templates/ldap_server.conf
+
+certtool -p --outfile /etc/ssl/private/ca_server.key
+certtool -s --load-privkey /etc/ssl/private/ca_server.key --template /etc/ssl/templates/ca_server.conf --outfile /etc/ssl/certs/ca_server.pem
+certtool -p --sec-param high --outfile /etc/ssl/private/ldap_server.key
+certtool -c --load-privkey /etc/ssl/private/ldap_server.key --load-ca-certificate /etc/ssl/certs/ca_server.pem \
+            --load-ca-privkey /etc/ssl/private/ca_server.key --template /etc/ssl/templates/ldap_server.conf --outfile /etc/ssl/certs/ldap_server.pem
+
+usermod -aG ssl-cert openldap
+chown :ssl-cert /etc/ssl/private/ldap_server.key
+chmod 640       /etc/ssl/private/ldap_server.key
+
+
+
+
+
+service slapd start
+
+echo $LDAP_CERT_CN
+
+# stimulate docker a bit
+while true; do sleep 60; done
