cc56b87b54bc9419eff42302afe331676cb7dcff
Wilfried Goesgens <willi@arangodb.com>
2017-04-12 17:24:47 +0200

    add first versions of a debian stretch buildcontainer

---
+++ distros/debian/stretch/build/scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+if test -n "${HTTP_PROXY}"; then
+    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+fi
+
+echo "proxy=${HTTP_PROXY}"
+apt-get update
+apt-get install -y tar wget bzip2 automake autoconf python-argparse build-essential apt-utils apt-transport-https debhelper libjemalloc-dev libssl-dev python python-pip
+
+pip install lockfile
+
+cd /tmp/
+# wget http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0//Release.key
+# apt-key add - < Release.key
+# rm Release.key
+
+# echo 'deb http://ftp.debian.org/debian jessie-backports main' | tee /etc/apt/sources.list.d/backports.list
+# echo 'deb http://download.opensuse.org/repositories/home:/fceller2/Debian_8.0/ /' | tee /etc/apt/sources.list.d/arangodbbuild.list
+apt-get update
+apt-get install -y gcc-5
+apt-get install -y git
+apt-get install -y cmake 
+
+useradd jenkins -u 1000
+
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/profile
---
+++ distros/debian/stretch/build/scripts/prepare_buildenv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+CC="/opt/arangodb/bin/gcc"
+CXX="/opt/arangodb/bin/g++"
