be0e8df17d13fb8f3d71adad945ef8d766bddee4
Wilfried Goesgens <willi@arangodb.com>
2017-07-26 16:58:50 +0200

    Add new ubuntu

---
+++ distros/ubuntu/17.04/run/scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+if test -n "${HTTP_PROXY}"; then
+    echo "proxy=${HTTP_PROXY}" >> /etc/yum.conf
+fi
+
+echo "proxy=${HTTP_PROXY}"
+apt-get update
+apt-get install -y \
+        tar curl bzip2 \
+        build-essential apt-utils apt-transport-https \
+        git cmake automake autoconf \
+        libjemalloc1 openssl \
+        python-lockfile \
+        \
+        gdb \
+        \
+        ruby-httparty \
+        ruby-multi-xml ruby-thread-safe rubygems-integration ruby-diff-lcs ri ruby-rgen
+
+gem install bundler
+curl -O https://raw.githubusercontent.com/arangodb/arangodb/devel/UnitTests/HttpInterface/Gemfile
+bundle
+
+useradd jenkins -u 1000
+
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/bashrc
+echo 'PATH=/opt/arangodb/bin/:${PATH}' >> /etc/profile
